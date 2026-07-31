1170eeefc605bc7f91e82d177d60f376a76b215b
Natalia Kur <natalia.kur@yahoo.com>
2026-07-31 20:16:43 +0200

    Add functionality to calculate total space used

--- 03/app.js
+++ 03/app.js
@@ -1,2 +1,58 @@
-import {files} from './data.js';
+import { files } from "./data.js";
+import "./style.css";
+const calculateBtn = document.querySelector(".calculate-btn");
+const usedSpaceDisplay = document.querySelector(".used-space");
+const resetBtn = document.querySelector(".reset-btn");
 
+const convertToMB = (length, unit) => {
+  let sizeInMB;
+  if (unit === "B") {
+    sizeInMB = length * 0.00000095;
+  }
+  if (unit === "KB") {
+    sizeInMB = length * 0.00097656;
+  }
+  if (unit === "MB") {
+    sizeInMB = length;
+  }
+  if (unit === "GB") {
+    sizeInMB = length * 1024;
+  }
+  return sizeInMB;
+};
+const calculateUsedSpace = () => {
+  let spaceUsed = 0;
+  files.forEach((file) => {
+    const { name, size } = file;
+    const { length, unit = "B" } = size;
+    const sizeInMB = convertToMB(length, unit);
+
+    // let sizeInMB;
+
+    // if (unit === "B") {
+    //   sizeInMB = length * 0.00000095;
+    // }
+    // if (unit === "KB") {
+    //   sizeInMB = length * 0.00097656;
+    // }
+    // if (unit === "MB") {
+    //   sizeInMB = length;
+    // }
+    // if (unit === "GB") {
+    //   sizeInMB = length * 1024;
+    // }
+    spaceUsed += sizeInMB;
+  });
+  usedSpaceDisplay.innerText = `Total used space: ${spaceUsed.toFixed(2)} MB`;
+  resetBtn.classList.remove("hidden");
+};
+
+const formatUnits = () => {};
+
+const resetDisplay = () => {
+  usedSpaceDisplay.innerText = "";
+  resetBtn.classList.add("hidden");
+};
+
+calculateBtn.addEventListener("click", calculateUsedSpace);
+resetBtn.addEventListener("click", resetDisplay);
